5328fafa83e5e27eb6e3895677fed1c51c58b1d2
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2024-02-05 08:25:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -12,11 +12,6 @@ const data=[
     heading:"burger",
     content:`A classic burger features a succulent patty, typically made from ground beef, grilled to perfection. Nestled in a soft, toasted bun, it's topped with crisp lettuce, juicy tomatoes, and your choice of condiments, creating a satisfying handheld delight. `,
 },{
-    url:'https://www.pngall.com/wp-content/uploads/5/Noodles-PNG-Image.png',
-    alt:'noodles',
-    heading:'Noodles',
-    content:`Noodles, a versatile and globally loved dish, come in various forms, from comforting ramen to stir-fried lo mein. Boiled or pan-fried, they serve as a perfect canvas for an array of vibrant vegetables, proteins, & savory sauces, creating a quick and flavorful meal.`
-},{
     url:chicken_tikka,
     content:`Immerse chicken chunks in a blend of yogurt and spices, letting them soak up the essence of cumin, coriander, and garam masala. Grill to perfection for a juicy and flavorful Chicken Tikka experience.Achieve the ideal smoky char by grilling the marinated chicken.`,
     heading:'chicken_tikka',
